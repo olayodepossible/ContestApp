15fb13bcb5a5c841837092edc1ae76db66b32fe4
Abayomi Akinfemiwa <olayodepossible@gmail.com>
2023-03-22 11:14:58 +0100

    project setup

---
+++ tests/Feature/ContestRegistrationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ContestRegistrationTest extends TestCase
+{
+    /** @test */
+
+    public function an_email_can_be_entered(){
+        
+    }
+}
